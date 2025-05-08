119a67f02eb56c97494d6df6698f6b17beee0861
lihe <lihe@jiayinfintech.cn>
2025-05-08 22:38:34 +0800

    kami

---
+++ components/tentangkami/Bantuanhukum.vue
@@ -0,0 +1,88 @@
+<!--
+ * @Description: 
+ * @version: 
+ * @Author: hean
+ * @Date: 2025-05-08 22:07:22
+ * @LastEditors: hean
+ * @LastEditTime: 2025-05-08 22:37:23
+-->
+<!--
+ * @Description: 
+ * @version: 
+ * @Author: hean
+ * @Date: 2025-05-08 21:30:13
+ * @LastEditors: hean
+ * @LastEditTime: 2025-05-08 22:01:13
+-->
+<template>
+  <section id="story" class="xl:px-40 md:-mt-32 xl:-mt-32">
+    <div class="bg-[#F5FBFD] rounded-2xl mt-4 relative">
+        <div id="laptop-story" class="hidden md:block md:pb-8 md:px-4 xl:pb-20 xl:px-20 relative">
+            <div >
+                <div class="flex justify-center gap-4 max-w-[880px] w-full mx-auto items-start">
+                    <div  class="mt-5 flex-1">
+                        <NuxtImg
+                        alt="story-2020"
+                        class="w-[280px] md:w-[410px] ml-auto" 
+                        src="/image/about-us/story-2020.svg" />
+                    </div>
+                    <div class="flex flex-col">
+                        <div class="relative w-4 h-4 md:w-7 md:h-7 rounded-full flex items-center justify-center mt-1 bg-blue-2">
+                            <div  class="w-2 h-2 md:w-4 md:h-4 bg-white rounded-full flex items-center justify-center">
+                            </div>
+                        </div>
+                        <div id="border-dashed" class="ml-1.5 mt-0.5 md:ml-3 border-l-2 border-dashed border-lightgrey-5 h-52 md:h-64">
+                        </div>
+                    </div>
+                    <div  class="flex-1 flex flex-col text-left">
+                        <span class="font-dm-sans font-bold md:text-[32px] text-gray-700 antialiased">2020</span>
+                        <pre class="font-dm-sans text-sm wrap text-pretty text-gray-500 antialiased mt-2 max-w-[300px] md:leading-6">                              Perqara lahir sebagai harapan untuk menjadi sebuah solusi inovatif terhadap minimnya akses ke bantuan hukum di Indonesia.
+                        Founder dan CEO Perqara, Yakup Putra Hasibuan, memulai Perqara dengan tujuan ingin menjadi platform hukum pertama yang menyediakan layanan konsultasi hukum gratis atau pro bono.
+                        </pre>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <div class="flex justify-center gap-4 max-w-[880px] w-full mx-auto items-start flex-row-reverse">
+                    <div  class="mt-5 flex-1">
+                        <NuxtImg
+                        alt="story-2021"
+                        class="w-[280px] md:w-[410px] mr-auto"
+                        src="/image/about-us/story-2021.svg" />
+                    </div>
+                    <div class="flex flex-col">
+                        <div  class="relative w-4 h-4 md:w-7 md:h-7 rounded-full flex items-center justify-center mt-1 bg-blue-2">
+                            <div class="w-2 h-2 md:w-4 md:h-4 bg-white rounded-full flex items-center justify-center"></div>
+                        </div>
+                        <div  id="border-dashed" class="ml-1.5 mt-0.5 md:ml-3 border-l-2 border-dashed border-lightgrey-5 h-52 md:h-64"></div>
+                    </div>
+                    <div class="flex-1 flex flex-col text-right items-end"><span class="font-dm-sans font-bold md:text-[32px] text-gray-700 antialiased">2021</span>
+                    <pre class="font-dm-sans text-sm wrap text-pretty text-gray-500 antialiased mt-2 max-w-[300px] md:leading-6">Kami dengan bangga menyambut kehadiran gedung kantor pertama kami di Senopati, Jakarta Selatan. Ini adalah titik awal pencapaian kami dan awal dari pertumbuhan yang berkelanjutan.
+                    </pre>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <div class="flex justify-center gap-4 max-w-[880px] w-full mx-auto items-start"><div  class="mt-5 flex-1">
+                    <NuxtImg  class="w-[280px] md:w-[410px] ml-auto" src="/image/about-us/story-2022.svg" alt="story-2020" /></div><div  class="flex flex-col"><div  class="relative w-4 h-4 md:w-7 md:h-7 rounded-full flex items-center justify-center mt-1 bg-blue-2"><div  class="w-2 h-2 md:w-4 md:h-4 bg-white rounded-full flex items-center justify-center"></div></div><div  id="border-dashed" class="ml-1.5 mt-0.5 md:ml-3 border-l-2 border-dashed border-lightgrey-5 h-52 md:h-64"></div></div><div  class="flex-1 flex flex-col text-left"><span  class="font-dm-sans font-bold md:text-[32px] text-gray-700 antialiased">2022</span><pre  class="font-dm-sans text-sm wrap text-pretty text-gray-500 antialiased mt-2 max-w-[300px] md:leading-6"> Pada Mei 2022 kami berhasil mengumumkan peluncuran Minimum Viable Product (MVP) kami berupa pemberian layanan konsultasi hukum pro bono melalui platform web.
+                </pre></div></div>
+            </div>
+
+            <div>
+                <div class="flex justify-center gap-4 max-w-[880px] w-full mx-auto items-start flex-row-reverse"><div class="mt-5 flex-1"><NuxtImg  class="w-[280px] md:w-[410px] mr-auto" src="/image/about-us/story-2023.svg" alt="story-2023" /></div><div  class="flex flex-col"><div  class="relative w-4 h-4 md:w-7 md:h-7 rounded-full flex items-center justify-center mt-1 bg-red-600"><div  class="w-2 h-2 md:w-4 md:h-4 bg-white rounded-full flex items-center justify-center"></div></div><!----></div><div  class="flex-1 flex flex-col text-right items-end"><span  class="font-dm-sans font-bold md:text-[32px] text-gray-700 antialiased">2023</span><pre  class="font-dm-sans text-sm wrap text-pretty text-gray-500 antialiased mt-2 max-w-[300px] md:leading-6">                              Perqara berhasil merilis secara resmi aplikasi konsultasi hukum yang mengakselerasikan pengalaman berkonsultasi hukum yang mudah dan efisien. 
+                Menjangkau lebih dari 120 kota di Indonesia, Perqara hadir sebagai sebuah platform yang memungkinan siapa pun dapat mengakses layanan hukum kapan pun dan di mana pun.
+                </pre></div></div>
+            </div>
+        </div>
+    </div>
+  </section>
+
+
+</template>
+<style scoped>
+    .bg-radial-gradient {
+        background: radial-gradient(50% 50% at 50% 50%,#07346a 0,#0a50a3)
+    }
+</style>
--- components/tentangkami/Section.vue
+++ components/tentangkami/Section.vue
@@ -4,15 +4,17 @@
  * @Author: hean
  * @Date: 2025-05-08 21:49:42
  * @LastEditors: hean
- * @LastEditTime: 2025-05-08 21:51:43
+ * @LastEditTime: 2025-05-08 22:07:56
 -->
 <template>
     <div>
         <Trusted></Trusted>
+        <Bantuanhukum></Bantuanhukum>
     </div>
 </template>
 
 <script setup>
     import Trusted from "./Trusted.vue";
+    import Bantuanhukum from "./Bantuanhukum.vue";
 
 </script>
