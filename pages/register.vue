89c2a32203975f4cfd85dd84bb2e708f3f1da8b0
lihe <lihe@jiayinfintech.cn>
2025-05-10 10:41:30 +0800

    register

--- pages/register.vue
+++ pages/register.vue
@@ -4,7 +4,7 @@
  * @Author: hean
  * @Date: 2025-05-10 09:39:46
  * @LastEditors: hean
- * @LastEditTime: 2025-05-10 10:17:36
+ * @LastEditTime: 2025-05-10 10:37:28
 -->
 <!--
  * @Description: 
@@ -25,7 +25,19 @@
                     <div class="flex items-center justify-between py-2 px-4 sm:px-0 mb-3"><button><svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 18.3102C11.0975 18.7008 11.0975 19.3339 10.707 19.7244C10.3164 20.115 9.68327 20.115 9.29274 19.7244L3.49985 13.9315C2.7188 13.1505 2.7188 11.8842 3.49985 11.1031L9.29274 5.31023C9.68327 4.9197 10.3164 4.9197 10.707 5.31023C11.0975 5.70075 11.0975 6.33392 10.707 6.72444L5.91406 11.5173H19.9998C20.5521 11.5173 20.9998 11.965 20.9998 12.5173C20.9998 13.0696 20.5521 13.5173 19.9998 13.5173H5.91406L10.707 18.3102Z" fill="#262E35"></path></svg></button><div class="font-semibold sm:text-base text-sm"> Punya akun Perqara? <a href="/login" class=""><button class="ml-2 sm:h-10 h-8 bg-[#F3FBFF] rounded-md sm:px-4 px-3 sm:text-sm text-xs font-semibold text-blue-2 hover:bg-white hover:text-[#1192E8] transition-colors"> Masuk </button></a></div></div>
 
                     <div flex lg:flex-row flex-col gap-6>
-                        
+                        <div class="lg:bg-[#E3F4FD]/80 lg:rounded-2xl lg:px-20 sm:px-0 px-4 lg:max-h-[800px] overflow-hidden flex-1">
+                        </div>
+
+                        <div class="lg:w-[386px] space-y-3">
+                            <div id="form-register" class="p-4 md:p-6 rounded-2xl bg-white md:shadow-[0_20px_50px_0_rgba(0,0,0,0.13)]">
+                                <p class="text-center text-neutral-800 text-2xl font-bold pb-4"> Daftar Akun </p>
+                                <div class="space-y-4">
+                                    <div class="space-y-2">
+                                        <label class="text-slate-700 text-xs">Nama Lengkap</label><div><input value="" type="text" class="border rounded-lg text-black border-gray-300 w-full" placeholder="Tuliskan nama lengkap Anda"><!----></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
