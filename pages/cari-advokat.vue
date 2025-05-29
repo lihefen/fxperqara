f383215eb1035936d2be156db930ab74c40fded3
lihe <lihe@jiayinfintech.cn>
2025-05-29 22:10:25 +0800

    律师列表

--- pages/cari-advokat.vue
+++ pages/cari-advokat.vue
@@ -133,45 +133,50 @@
                     <div class="lg:col-span-2">
                         <!-- <CariSwiper /> -->
                         <div class="mb-4">
-                            <div>
+                            <div v-for="(item,index) in advokatList" :key="index + 908">
                                 <div class="cursor-pointer relative font-lexend min-w-[328px] max-h-[239px] mb-4">
                                     <div class="relative bg-white rounded-lg overflow-hidden border border-[#E7ECF0]">
                                         <div class="relative md:py-3 py-2">
                                             <div class="absolute left-2 top-2 z-[4]"><span class="relative flex h-3 w-3"><span class="absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75 bg-green-200 animate-ping-slow"></span><span class="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span></span></div>
 
-                                            <div class="md:px-3 px-2 flex gap-3">
+                                            <div class="md:px-3 px-2 items-center flex gap-3">
                                                 <div class="relative h-fit">
-                                                    <img src="/image/photo/photo_1722932861.jpeg" class="min-w-[62px] w-[62px] md:w-[80px] max-w-[80px] min-h-[80px] h-[80px] md:h-[123px] max-h-[123px] rounded-md md:rounded-lg md:ml-1 object-cover border border-[#E7ECF0]">
-                                                    <!-- <div id="tagging-video-voice-call" class="absolute bottom-0 bg-gradient-to-r from-[#52BEE7] to-[#1C2E7F] min-w-[62px] w-[62px] md:w-[90px] max-w-[90px] flex items-center justify-center gap-1 py-1 px-2 md:py-2 md:px-3 rounded-md md:rounded-lg">
-                                                        <img
-                                                        alt="ivideo-icon"
-                                                        class="w-2 md:w-4"
-                                                        src="/image/lawyer-card/video-icon.png" />
-                                                        <p class="text-[8px] md:text-xs text-center text-white font-semibold antialiased"> Tersedia </p>
-                                                    </div> -->
+                                                    <img :src="item.photo" class="min-w-[62px] w-[62px] md:w-[80px] max-w-[80px] min-h-[80px] h-[80px] md:h-[123px] max-h-[123px] rounded-md md:rounded-lg md:ml-1 object-cover border border-[#E7ECF0]">
                                                 </div>
-                                                <div class="flex-1 flex flex-col gap-1.5">
+                                                <div class=" flex flex-col gap-1.5 flex-1">
                                                     <div class="flex items-center h-[40px] md:h-[52px]">
                                                         <span class="text-sm md:text-xl font-bold line-clamp-2 leading-4 md:leading-6">Dwi Kurniawan, S.H.</span>
                                                     </div>
                                                     <div class="flex gap-1 text-xs md:text-sm items-center">
                                                         <div class="flex gap-1 items-center">
                                                             <img
-                                                                alt="location"
-                                                                class="w-4 h-4"
-                                                                src="/image/landing-page/icon-location.png" />
-                                                            <span class="text-[#4D5B6A] capitalize">bogor</span>
+                                                            alt="medal-star"
+                                                            class="w-4 h-4"
+                                                            src="/image/landing-page/icon-location-1.png" />
+                                                            <span class="text-[#333333] capitalize">Jember</span>
                                                         </div>
                                                         <div class="border-l h-2/3 border-gray-500"></div><div class="flex gap-1 flex-1">
                                                             <img
                                                                 alt="medal-star"
                                                                 class="w-4 h-4"
                                                                 src="/image/landing-page/icon-medal-star.png" />
-                                                                <span class="text-[#0072C3] truncate">Terverifikasi oleh Peradi</span>
+                                                                <span class="text-[#333333] truncate">Verified by Peradi</span>
                                                         </div>
                                                     </div>
                                                     <div class="flex items-center">
-                                                        <p class="text-xs md:text-sm antialiased text-[#344052] my-auto flex gap-1 items-center !leading-4"><svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.3026 4.07139C11.8068 3.52306 10.9784 3.24889 9.77677 3.24889H9.63677V3.22556C9.63677 2.24556 9.63677 1.03223 7.44344 1.03223H6.55677C4.36344 1.03223 4.36344 2.25139 4.36344 3.22556V3.25473H4.22344C3.01594 3.25473 2.19344 3.52889 1.6976 4.07723C1.1201 4.71889 1.1376 5.58223 1.19594 6.17139L1.20177 6.21223L1.24695 6.68664C1.25527 6.77401 1.30233 6.85293 1.37576 6.90098C1.51565 6.99252 1.74977 7.14343 1.8901 7.22139C1.97177 7.27389 2.05927 7.32056 2.14677 7.36723C3.14427 7.91556 4.24094 8.28306 5.3551 8.46389C5.4076 9.01223 5.64677 9.65389 6.92427 9.65389C8.20177 9.65389 8.4526 9.01806 8.49344 8.45223C9.68344 8.25973 10.8326 7.84556 11.8709 7.23889C11.9059 7.22139 11.9293 7.20389 11.9584 7.18639C12.1899 7.05559 12.4299 6.89442 12.6488 6.73655C12.7147 6.68899 12.7569 6.61546 12.7659 6.53466L12.7751 6.45139L12.8043 6.17723C12.8101 6.14223 12.8101 6.11306 12.8159 6.07223C12.8626 5.48306 12.8509 4.67806 12.3026 4.07139ZM7.63594 8.06723C7.63594 8.68556 7.63594 8.77889 6.91844 8.77889C6.20094 8.77889 6.20094 8.66806 6.20094 8.07306V7.33806H7.63594V8.06723ZM5.1976 3.24889V3.22556C5.1976 2.23389 5.1976 1.86639 6.55677 1.86639H7.44344C8.8026 1.86639 8.8026 2.23973 8.8026 3.22556V3.25473H5.1976V3.24889Z" fill="#0072C3"></path><path d="M12.1759 8.01136C12.3822 7.91321 12.6198 8.0767 12.5991 8.30419L12.3897 10.6106C12.2672 11.7773 11.7889 12.9673 9.22225 12.9673H4.77725C2.21058 12.9673 1.73225 11.7773 1.60975 10.6164L1.411 8.43019C1.39056 8.20534 1.6227 8.04203 1.8284 8.13509C2.49306 8.43581 3.71989 8.96932 4.47774 9.16781C4.57359 9.19292 4.65111 9.26152 4.69308 9.35127C5.04715 10.1085 5.81506 10.5114 6.92391 10.5114C8.02181 10.5114 8.79942 10.093 9.15466 9.33389C9.19669 9.24408 9.27429 9.1755 9.37019 9.15026C10.1749 8.93851 11.4808 8.34215 12.1759 8.01136Z" fill="#0072C3"></path></svg> 6 Tahun </p><div class="flex items-center"><div class="h-2 border-l border-gray-400 mx-2"></div><svg viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-[14px] h-[14px] md:w-4 md:h-4"><path fill-rule="evenodd" clip-rule="evenodd" d="M6.29311 1.87199C6.55464 1.24319 7.44541 1.24319 7.70694 1.87199L8.92144 4.79199L12.0738 5.04472C12.7527 5.09914 13.0279 5.94631 12.5107 6.38935L10.1089 8.44674L10.8427 11.5229C11.0007 12.1854 10.2801 12.7089 9.69891 12.354L7.00003 10.7055L4.30115 12.354C3.71996 12.7089 2.99931 12.1854 3.15733 11.5229L3.89111 8.44674L1.48933 6.38935C0.972122 5.94631 1.24738 5.09914 1.92623 5.04472L5.07861 4.79199L6.29311 1.87199Z" fill="#FEAF27"></path></svg><label class="my-auto ml-0.5 text-xs md:text-sm text-black">4.9</label></div><span class="text-xs md:text-sm text-[#757E8C] ml-1">(346 Konsultasi)</span>
+                                                        <p class="text-xs md:text-sm antialiased text-[#344052] my-auto flex gap-1 items-center !leading-4">
+                                                            <img
+                                                            alt="medal-star"
+                                                            class="w-4 h-4"
+                                                            src="/image/landing-page/icon-year.png" />6 years 
+                                                        </p>
+                                                        <div class="flex items-center">
+                                                            <div class="h-2 border-l border-gray-400 mx-2"></div>
+                                                            
+                                                            <img
+                                                            alt="medal-star"
+                                                            class="w-4 h-4"
+                                                            src="/image/landing-page/icon-score.png" /><label class="my-auto ml-0.5 text-xs md:text-sm text-black">4.9</label></div><span class="text-xs md:text-sm text-[#757E8C] ml-1">（29 consultations）</span>
                                                     </div>
                                                     <div class="flex items-center justify-between gap-1 md:gap-2">
                                                         <div class="flex text-xs md:text-sm justify-between">
@@ -179,24 +184,18 @@
                                                                 <span class="text-[#04A45E] bg-[#CBFFE8] text-xs md:text-sm leading-3 w-auto rounded px-1 md:px-2 py-0.5 md:py-1 self-start max-w-[150px] truncate">Criminal</span><span class="text-[#04A45E] bg-[#CBFFE8] text-xs md:text-sm leading-3 w-auto rounded px-1 md:px-2 py-0.5 md:py-1 self-start max-w-[150px] truncate">Civil</span><span class="text-[#04A45E] bg-[#CBFFE8] text-xs md:text-sm leading-3 w-auto rounded px-1 md:px-2 py-0.5 md:py-1 self-start max-w-[150px] truncate">Employment</span>
                                                             </div>
                                                         </div>
-                                                        <!-- <div class="flex items-center gap-2 text-xs md:text-sm">
-                                                            <span>+2</span><svg class="svg-inline--fa fa-chevron-down text-xs md:text-sm" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path class="" fill="currentColor" d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"></path></svg>
-                                                        </div> -->
                                                     </div>
                                                 </div>
-                                                <!-- flex justify-between items-center gap-1 md:px-3 px-2 -->
-                                                <div>
+                                                <div class="min-w-[200px]">
                                                     <div class="">
                                                         <div class="flex items-center gap-2">
                                                             <div class="flex flex-col gap-0.5 md:gap-1 rounded-lg">
                                                                 <div class="text-xs text-[#757E8C]">Starting from</div>
                                                                 <div class="flex gap-1 items-center"><p class="text-[#04A45E] font-semibold !leading-5 text-sm md:text-lg">Rp40.000</p><p class="text-xs line-through text-[#9B9B9B]">Rp60.000</p>
-                                                                    <!-- <div class="flex items-center justify-center w-[15px] bg-[#FF5B5E] rounded px-1 py-0.5 text-white text-[8px] leading-[10px]"> % 
-                                                                    </div> -->
                                                                 </div>
                                                             </div>
                                                         </div>
-                                                        <button class="font-medium px-4 py-2.5 md:px-6 md:py-3 md:w-32 rounded-md md:rounded-lg text-xs md:text-base bg-[#04A45E] text-white">Konsultasi</button>
+                                                        <button class="font-medium px-4 py-2.5 md:px-6 md:py-3 md:w-40 rounded-md md:rounded-[16px] text-xs md:text-base bg-[#04A45E] mt-[10px] text-white">Konsultasi</button>
                                                     </div>
                                                 </div>
                                             </div>
@@ -272,6 +271,22 @@ const kotaOptions = [
         label: 'ACEH TENGGARA',
     },
     
+]
+
+
+const advokatList = [
+    {
+        photo:'/image/photo/photo-01.png'
+    },
+    {
+        photo:'/image/photo/photo-02.png'
+    },
+    {
+        photo:'/image/photo/photo-03.png'
+    },
+    {
+        photo:'/image/photo/photo-04.png'
+    }
 ]
 
 </script>
