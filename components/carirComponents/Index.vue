333728a7e1781d59f9bf4a6ef162e8f7fd071945
lihe <lihe@jiayinfintech.cn>
2025-06-01 10:45:15 +0800

    官网

--- components/carirComponents/Index.vue
+++ components/carirComponents/Index.vue
@@ -4,11 +4,12 @@
  * @Author: hean
  * @Date: 2025-05-31 14:46:30
  * @LastEditors: hean
- * @LastEditTime: 2025-06-01 10:08:47
+ * @LastEditTime: 2025-06-01 10:42:36
 -->
 <template>
   <section>
-   <div class="bg-white">
+   <div class="bg-white pb-8">
+        <Work></Work>
         <Ours></Ours>
         <Solusi />
     </div>
@@ -21,4 +22,6 @@
 import Solusi from "./Solusi.vue";
 import Careers from "./Careers.vue";
 import Ours from "./Ours.vue"
+import Work from "./Work.vue"
+
 </script>
